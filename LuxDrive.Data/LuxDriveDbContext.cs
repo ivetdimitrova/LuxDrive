90321474977031054a0a200831300f0b12db6ebc
ivetdimitrova <ivetdimitrova28@gmail.com>
2025-10-05 11:59:20 +0300

    Add constructor in DbContext

--- LuxDrive.Data/LuxDriveDbContext.cs
+++ LuxDrive.Data/LuxDriveDbContext.cs
@@ -1,8 +1,13 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 
 namespace LuxDrive.Data
 {
     public class LuxDriveDbContext : IdentityDbContext
     {
+        public LuxDriveDbContext(DbContextOptions<LuxDriveDbContext> options)
+          : base(options)
+        {
+        }
     }
 }
