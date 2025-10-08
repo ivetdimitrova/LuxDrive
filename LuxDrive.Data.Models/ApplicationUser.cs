9c9259aeede7a0604f2bd8fbf4909fbadc0305d1
ivetdimitrova <ivetdimitrova28@gmail.com>
2025-10-08 16:47:54 +0300

    Add database models

---
+++ LuxDrive.Data.Models/ApplicationUser.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace LuxDrive.Data.Models
+{
+    public class ApplicationUser : IdentityUser<Guid>
+    {
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+
+        public virtual ICollection<File> Files { get; set; }
+         = new HashSet<File>();
+    }
+}
---
+++ LuxDrive.Data.Models/File.cs
@@ -0,0 +1,14 @@
+﻿namespace LuxDrive.Data.Models
+{
+    public class File
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string Extension { get; set; } = null!;
+        public long Size { get; set; }
+        public string StorageUrl { get; set;} = null!;
+        public DateTime UploadAt { get; set; }
+
+        public virtual ApplicationUser User { get; set; } = null!;
+    }
+}
