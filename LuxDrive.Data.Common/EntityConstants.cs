ccad6ccec183a33b4d3ef7739494cd1041fed0ce
ivetdimitrova <ivetdimitrova28@gmail.com>
2025-10-08 17:08:57 +0300

    Add comments to table's columns

--- LuxDrive.Data.Common/EntityConstants.cs
+++ LuxDrive.Data.Common/EntityConstants.cs
@@ -7,6 +7,10 @@
             public const int FirstNameLength = 100;
             public const int LastNameLength = 150;
 
+
+            //Table column comments
+            public const string FirstNameComment = "User's first name";
+            public const string LastNameComment = "User's last name";
         }
     }
 }
