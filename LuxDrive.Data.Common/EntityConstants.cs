d6b34bacf92ba96a49435358ff331ca4610b3d4d
ivetdimitrova <ivetdimitrova28@gmail.com>
2025-10-08 17:07:19 +0300

    Add entity constants for ApplicationUser

---
+++ LuxDrive.Data.Common/EntityConstants.cs
@@ -0,0 +1,12 @@
+﻿namespace LuxDrive.Data.Common
+{
+    public class EntityConstants
+    {
+        public class ApplicationUser
+        {
+            public const int FirstNameLength = 100;
+            public const int LastNameLength = 150;
+
+        }
+    }
+}
