867f921e7105fe0e2c1c24a367184d7c2a02979b
ivetdimitrova <ivetdimitrova28@gmail.com>
2025-10-08 17:22:01 +0300

    Update ApplicationUser model configuration
    This reverts commit 8a95ae338c0a58833fbbc7a09769ad86b44d0e25.

---
+++ LuxDrive.Data/Configurations/ApplicationUserConfiguration.cs
@@ -0,0 +1,31 @@
+﻿using LuxDrive.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+using static LuxDrive.Data.Common.EntityConstants.ApplicationUser;
+
+
+namespace LuxDrive.Data.Configurations
+{
+    public class ApplicationUserConfiguration : IEntityTypeConfiguration<ApplicationUser>
+    {
+        public void Configure(EntityTypeBuilder<ApplicationUser> entity)
+        {
+          entity
+                .HasKey(au => au.Id);
+
+            entity
+                .Property(au => au.FirstName)
+                .IsRequired()
+                .HasMaxLength(FirstNameLength)
+                .HasComment(FirstNameComment);
+
+            entity
+               .Property(au => au.LastName)
+               .IsRequired()
+               .HasMaxLength(LastNameLength)
+               .HasComment(LastNameComment);
+
+        }
+    }
+}
