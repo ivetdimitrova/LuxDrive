02c3e17ed447cfe0112976a33ef880b50900ce01
ivetdimitrova <ivetdimitrova28@gmail.com>
2025-10-09 17:15:09 +0300

    Add FIle model configuration

---
+++ LuxDrive.Data/Configurations/FileConfiguration.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using static LuxDrive.Data.Common.EntityConstants.File;
+
+using FileEntity = LuxDrive.Data.Models.File;
+
+namespace LuxDrive.Data.Configurations
+{
+    public class FileConfiguration : IEntityTypeConfiguration<FileEntity>
+    {
+        public void Configure(EntityTypeBuilder<FileEntity> entity)
+        {
+            entity
+                .HasKey(f => f.Id);
+
+            entity
+                .Property(f => f.Id)
+                .HasComment(IdComment);
+
+            entity
+                .Property(f => f.Name)
+                .IsRequired()
+                .HasMaxLength(NameLength)
+                .HasComment(NameComment);
+
+            entity
+                .Property(f => f.Extension)
+                .IsRequired()
+                .HasMaxLength(ExtensionLength)
+                .HasComment(ExtensionComment);
+
+            entity
+                .Property(f=>f.Size)
+                .HasComment(SizeComment);
+
+            entity
+                .Property(f=>f.StorageUrl)
+                .HasComment(StorageUrlComment);
+
+            entity
+                .Property(f=>f.UploadAt)
+                .HasComment(UploadAtComment);
+
+            entity
+                .Property(f=>f.UserId)
+                .HasComment(UserIdComment); 
+
+            entity
+                .HasOne(f=>f.User)
+                .WithMany(u=>u.Files)
+                .HasForeignKey(f => f.UserId);
+             
+        }
+    }
+}
