87d632c7b20a6927bd8414491db0b07bd52f0554
Gabriel Yonov <157459005+gabrielyonov15@users.noreply.github.com>
2025-10-19 15:02:24 +0300

    Added Pricing page and controller

---
+++ LuxDrive/Views/Pricing/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LuxDrive.Views.Pricing
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
