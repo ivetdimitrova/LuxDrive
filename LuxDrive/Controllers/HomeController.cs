eb3053451a629fd8ea45df8781e2cee061acea8a
ivetdimitrova <ivetdimitrova28@gmail.com>
2025-10-19 16:01:38 +0300

    Scaffold Identity

--- LuxDrive/Controllers/HomeController.cs
+++ LuxDrive/Controllers/HomeController.cs
@@ -29,5 +29,10 @@ namespace LuxDrive.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult GetStart()
+        {
+            return View();
+        }
     }
 }
