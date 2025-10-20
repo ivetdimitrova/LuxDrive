152215061c14e8e282b2f0329e16b9ca6eb9e480
ivetdimitrova <ivetdimitrova28@gmail.com>
2025-10-20 16:42:16 +0300

    Resolve missing UserManager<ApplicationUser> registration

--- LuxDrive/Program.cs
+++ LuxDrive/Program.cs
@@ -12,8 +12,6 @@ var connectionString = builder.Configuration.GetConnectionString("DefaultConnect
 builder.Services.AddDbContext<LuxDriveDbContext>(options =>
     options.UseSqlServer(connectionString));
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<LuxDriveDbContext>();
-
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddIdentity<ApplicationUser, IdentityRole<Guid>>(options => options.SignIn.RequireConfirmedAccount = true)
