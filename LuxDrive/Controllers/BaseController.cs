6c5d050f151d01163b9a5c09cdc2bd15a1b2f4e6
ivetdimitrova <ivetdimitrova28@gmail.com>
2025-10-21 19:33:16 +0300

    Create BaseController

---
+++ LuxDrive/Controllers/BaseController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace LuxDrive.Controllers
+{
+    public class BaseController : Controller
+    {
+        protected bool IsUserAuthenticated()
+        {
+            bool retRes = false;
+
+            if (this.User.Identity != null)
+            {
+                retRes = this.User.Identity.IsAuthenticated;
+            }
+
+            return retRes;
+        }
+        protected string? GetUserId()
+        {
+            string? userId = null;
+
+            if (this.IsUserAuthenticated())
+            {
+                userId = this.User.FindFirstValue(ClaimTypes.NameIdentifier);
+            }
+
+            return userId;
+        }
+    }
+}
