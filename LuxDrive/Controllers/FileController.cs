8ba6687d1c5d684bb637780231431fbe4261a755
ivetdimitrova <ivetdimitrova28@gmail.com>
2025-10-21 19:38:22 +0300

    FileController is intended to be used by registered users.

--- LuxDrive/Controllers/FileController.cs
+++ LuxDrive/Controllers/FileController.cs
@@ -1,8 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace LuxDrive.Controllers
 {
-    public class FilesController : BaseController
+    [Authorize]
+    public class FileController : BaseController
     {
         public IActionResult Index()
         {
