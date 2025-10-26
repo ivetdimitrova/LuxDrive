20610f82580649be46362f16acf8b4e9fa71f4f7
Gabriel Yonov <157459005+gabrielyonov15@users.noreply.github.com>
2025-10-26 13:21:04 +0200

    Added Success view for payment confirmation

---
+++ LuxDrive/Views/Pricing/Success.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LuxDrive.Views.Pricing
+{
+    public class SuccessModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
