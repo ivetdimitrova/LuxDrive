a8c3ea9bd520f1150eb4e58126b2b6e3d9e9528b
Gabriel Yonov <157459005+gabrielyonov15@users.noreply.github.com>
2025-10-26 13:08:47 +0200

    Added Checkout view with basic payment form

---
+++ LuxDrive/Views/Pricing/Checkout.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LuxDrive.Views.Pricing
+{
+    public class CheckoutModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
