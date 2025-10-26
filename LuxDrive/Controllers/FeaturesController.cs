f50cd7be34d7793cbb20d1f7116bd0cd93020952
Gabriel Yonov <157459005+gabrielyonov15@users.noreply.github.com>
2025-10-26 15:22:22 +0200

    Add FeaturesController with basic Index action

---
+++ LuxDrive/Controllers/FeaturesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LuxDrive.Controllers
+{
+    public class FeaturesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
