404d81126fa838cee34aaf9516bb1f3107aaeae6
Gabriel Yonov <157459005+gabrielyonov15@users.noreply.github.com>
2025-12-08 18:04:06 +0200

    feat: Implement SpacesService and add endpoint URL

--- LuxDrive.Services/SpacesService..cs
+++ LuxDrive.Services/SpacesService..cs
@@ -6,74 +6,53 @@ namespace LuxDrive.Services
 {
     public class SpacesService
     {
-            private readonly string accessKey = "DO8013YB8VKAUKLVHYVQ";
-            private readonly string secretKey = "ECZMfQfsmhpZJfwwZpqWsW274VI+uJuS77pxAvAqoCM";
-            private readonly string bucketName = "luxdrive";
-            private readonly string region = "ams3";
-            private readonly IAmazonS3 client;
+        private readonly string accessKey = "DO8013YB8VKAUKLVHYVQ";
+        private readonly string secretKey = "ECZMfQfsmhpZJfwwZpqWsW274VI+uJuS77pxAvAqoCM";
+        private readonly string bucketName = "luxdrive";
+        private readonly string region = "ams3";
+        private readonly string endpointUrl = "https://luxdrive.ams3.digitaloceanspaces.com";
+        private readonly IAmazonS3 client;
 
-            public SpacesService()
+        public SpacesService()
+        {
+            var config = new AmazonS3Config
             {
-                var config = new AmazonS3Config
-                {
-                    ServiceURL = $"https://{region}.digitaloceanspaces.com",
-                    ForcePathStyle = true
-                };
+                ServiceURL = $"https://{region}.digitaloceanspaces.com",
+                ForcePathStyle = true
+            };
 
-                client = new AmazonS3Client(
-                    new BasicAWSCredentials(accessKey, secretKey),
-                    config
-                );
-            }
+            var credentials = new BasicAWSCredentials(accessKey, secretKey);
 
-            public async Task<string> UploadFile(Stream fileStream, string fileName)
+            client = new AmazonS3Client(credentials, config);
+        }
+
+        public async Task<string> UploadAsync(Stream stream, string key, string contentType)
+        {
+            var putRequest = new PutObjectRequest
             {
-                var request = new PutObjectRequest
-                {
-                    BucketName = bucketName,
-                    Key = fileName,
-                    InputStream = fileStream,
-                    CannedACL = S3CannedACL.PublicRead
-                };
+                BucketName = bucketName,
+                Key = key,
+                InputStream = stream,
+                ContentType = contentType,
+                CannedACL = S3CannedACL.PublicRead
+            };
 
-                await client.PutObjectAsync(request);
+            await client.PutObjectAsync(putRequest);
 
-                return $"https://{bucketName}.{region}.digitaloceanspaces.com/{fileName}";
-            }
+            // Публичен URL към файла
+            return $"{endpointUrl}/{key}";
+        }
 
-            public async Task DeleteFile(string fileName)
+        public async Task<List<string>> ListFiles()
+        {
+            var request = new ListObjectsV2Request
             {
-                var request = new DeleteObjectRequest
-                {
-                    BucketName = bucketName,
-                    Key = fileName
-                };
+                BucketName = bucketName
+            };
 
-                await client.DeleteObjectAsync(request);
-            }
+            var response = await client.ListObjectsV2Async(request);
 
-            public async Task<Stream> DownloadFile(string fileName)
-            {
-                var request = new GetObjectRequest
-                {
-                    BucketName = bucketName,
-                    Key = fileName
-                };
-
-                var response = await client.GetObjectAsync(request);
-                return response.ResponseStream;
-            }
-
-            public async Task<List<string>> ListFiles()
-            {
-                var request = new ListObjectsV2Request
-                {
-                    BucketName = bucketName
-                };
-
-                var response = await client.ListObjectsV2Async(request);
-
-                return response.S3Objects.Select(x => x.Key).ToList();
-            }
+            return response.S3Objects.Select(x => x.Key).ToList();
         }
     }
+}
