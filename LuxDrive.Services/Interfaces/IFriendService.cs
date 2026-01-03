7890bdd21b09ff27c2b5e4e288cbf1c4a8d5a1f5
Gabriel Yonov <157459005+gabrielyonov15@users.noreply.github.com>
2026-01-03 15:19:53 +0200

    Define interface for friend management and requests

---
+++ LuxDrive.Services/Interfaces/IFriendService.cs
@@ -0,0 +1,21 @@
+﻿using LuxDrive.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LuxDrive.Services.Interfaces
+{
+    public interface IFriendService
+    {
+        Task SendRequestAsync(Guid senderId, Guid receiverId);
+        Task AcceptRequestAsync(int requestId);
+        Task RejectRequestAsync(int requestId);
+        Task<IEnumerable<object>> GetPendingRequestsAsync(Guid userId);
+        Task<ApplicationUser?> FindUserByEmailAsync(string email);
+        Task<IEnumerable<object>> GetFriendsAsync(Guid userId);
+        Task RemoveFriendAsync(Guid userId, Guid friendId);
+        Task<IEnumerable<object>> GetSentPendingRequestsAsync(Guid userId);
+    }
+}
