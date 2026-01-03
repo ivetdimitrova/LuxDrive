a964c9a0a03a65667e5c1c07b3a60ea5f54afc8c
Gabriel Yonov <157459005+gabrielyonov15@users.noreply.github.com>
2026-01-03 15:03:52 +0200

    Implement SharedFile entity for file sharing feature

---
+++ LuxDrive.Data.Models/SharedFile.cs
@@ -0,0 +1,15 @@
+﻿namespace LuxDrive.Data.Models
+{
+    public class SharedFile
+    {
+        public int Id { get; set; }
+
+        public Guid FileId { get; set; }
+        public File File { get; set; } = null!;
+
+        public Guid SenderId { get; set; }
+        public Guid ReceiverId { get; set; }
+
+        public DateTime SharedOn { get; set; }
+    }
+}
