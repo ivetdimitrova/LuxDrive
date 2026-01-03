f2be4e54c9b1acdd2cdcc6055a5e78b8f4a74ee5
Gabriel Yonov <157459005+gabrielyonov15@users.noreply.github.com>
2026-01-03 15:03:04 +0200

    feat: add FriendRequest class for social system

---
+++ LuxDrive.Data.Models/FriendRequest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LuxDrive.Data.Models.Enums;
+
+    namespace LuxDrive.Data.Models
+    {
+        public class FriendRequest
+        {
+            public int Id { get; set; }
+
+            public Guid SenderId { get; set; }
+            public ApplicationUser Sender { get; set; } = null!;
+
+            public Guid ReceiverId { get; set; }
+            public ApplicationUser Receiver { get; set; } = null!;
+
+            public FriendRequestStatus Status { get; set; }
+
+            public DateTime CreatedOn { get; set; }
+        }
+    }
