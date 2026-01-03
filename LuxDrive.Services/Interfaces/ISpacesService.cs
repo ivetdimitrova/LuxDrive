a22890b905e44286073e056dacfe4673735bd977
Gabriel Yonov <157459005+gabrielyonov15@users.noreply.github.com>
2026-01-03 15:42:11 +0200

    Define interface for cloud storage operations

---
+++ LuxDrive.Services/Interfaces/ISpacesService.cs
@@ -0,0 +1,9 @@
+﻿namespace LuxDrive.Services.Interfaces
+{
+    public interface ISpacesService
+    {
+        Task<string> UploadAsync(Stream stream, string key, string contentType);
+        Task<List<string>> ListFiles();
+        Task DeleteAsync(string key);
+    }
+}
