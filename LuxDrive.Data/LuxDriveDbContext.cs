a3139633b63f86ddaff6ed77ae3f06562fa9cb01
Gabriel Yonov <157459005+gabrielyonov15@users.noreply.github.com>
2026-01-03 15:06:12 +0200

    Update LuxDriveDbContext with UserFriend and FriendRequest configuration

--- LuxDrive.Data/LuxDriveDbContext.cs
+++ LuxDrive.Data/LuxDriveDbContext.cs
@@ -1,23 +1,27 @@
 ﻿using LuxDrive.Data.Configurations;
 using LuxDrive.Data.Models;
+using LuxDrive.Data.Models.Enums;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 using FileEntity = LuxDrive.Data.Models.File;
 
-
 namespace LuxDrive.Data
 {
-    public class LuxDriveDbContext : IdentityDbContext<ApplicationUser, IdentityRole<Guid>, Guid>
+    public class LuxDriveDbContext
+        : IdentityDbContext<ApplicationUser, IdentityRole<Guid>, Guid>
     {
         public LuxDriveDbContext(DbContextOptions<LuxDriveDbContext> options)
-          : base(options)
+            : base(options)
         {
         }
 
         public virtual DbSet<FileEntity> Files { get; set; } = null!;
 
+        public DbSet<FriendRequest> FriendRequests { get; set; } = null!;
+        public DbSet<UserFriend> UserFriends { get; set; } = null!;
+        public DbSet<SharedFile> SharedFiles { get; set; } = null!;
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
@@ -26,7 +30,32 @@ namespace LuxDrive.Data
             builder.ApplyConfiguration(new ApplicationUserConfiguration());
             builder.ApplyConfiguration(new FileConfiguration());
 
-        }
+            builder.Entity<UserFriend>()
+                .HasKey(x => new { x.UserId, x.FriendId });
 
+            builder.Entity<UserFriend>()
+                .HasOne(x => x.User)
+                .WithMany()
+                .HasForeignKey(x => x.UserId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.Entity<UserFriend>()
+                .HasOne(x => x.Friend)
+                .WithMany()
+                .HasForeignKey(x => x.FriendId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.Entity<FriendRequest>()
+                .HasOne(x => x.Sender)
+                .WithMany()
+                .HasForeignKey(x => x.SenderId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.Entity<FriendRequest>()
+                .HasOne(x => x.Receiver)
+                .WithMany()
+                .HasForeignKey(x => x.ReceiverId)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
     }
 }
