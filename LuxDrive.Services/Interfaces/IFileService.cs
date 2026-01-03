f7a0953347143924dca59a2d0e0cf48ddab21b3c
Gabriel Yonov <157459005+gabrielyonov15@users.noreply.github.com>
2026-01-03 15:41:31 +0200

    Expand IFileService with Rename, Delete, and Retrieval methods

--- LuxDrive.Services/Interfaces/IFileService.cs
+++ LuxDrive.Services/Interfaces/IFileService.cs
@@ -8,5 +8,12 @@ namespace LuxDrive.Services.Interfaces
         Task<string?> GetFileExtensionAsync(Guid? fileId);
         Task<bool> UpdateFileUrlAsync(Guid? fileId, string url);
         Task ShareFileAsync(Guid fileId, Guid senderId, Guid receiverId);
+        Task<IEnumerable<Data.Models.File>> GetUserFilesAsync(Guid userId);
+
+        Task<bool> ChangeFileNameAsync(Guid userId,Guid fileId,string newName);
+
+        Task<Data.Models.File?> GetUserFileAsync(Guid fileId, Guid userId);
+
+        Task<bool> RemoveFileAsync(Data.Models.File file);
     }
 }
