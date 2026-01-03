11019fa4ac5cda6a92e82195d05effb93b3bde6a
Gabriel Yonov <157459005+gabrielyonov15@users.noreply.github.com>
2026-01-03 15:04:50 +0200

    Implement UserFriend entity for many-to-many relationship

---
+++ LuxDrive.Data.Models/UserFriend.cs
@@ -0,0 +1,11 @@
+﻿namespace LuxDrive.Data.Models
+{
+    public class UserFriend
+    {
+        public Guid UserId { get; set; }
+        public ApplicationUser User { get; set; } = null!;
+
+        public Guid FriendId { get; set; }
+        public ApplicationUser Friend { get; set; } = null!;
+    }
+}
