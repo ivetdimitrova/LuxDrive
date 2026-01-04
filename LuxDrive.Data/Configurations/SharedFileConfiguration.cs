eb5ebb827d8a8d007aad621d52065e817ca8adfe
Gabriel Yonov <157459005+gabrielyonov15@users.noreply.github.com>
2026-01-04 14:04:31 +0200

    Add SharedFile entity configuration with Restrict delete behavior

---
+++ LuxDrive.Data/Configurations/SharedFileConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using LuxDrive.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace LuxDrive.Data.Configurations
+{
+    public class SharedFileConfiguration : IEntityTypeConfiguration<SharedFile>
+    {
+        public void Configure(EntityTypeBuilder<SharedFile> builder)
+        {
+            builder.HasOne(sf => sf.Sender)
+                   .WithMany()
+                   .HasForeignKey(sf => sf.SenderId)
+                   .OnDelete(DeleteBehavior.Restrict);
+
+            builder.HasOne(sf => sf.Receiver)
+                   .WithMany()
+                   .HasForeignKey(sf => sf.ReceiverId)
+                   .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
