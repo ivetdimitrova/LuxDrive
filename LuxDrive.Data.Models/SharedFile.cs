d58567d4c59795871faf5f8f95acab8c68d6fbe1
Gabriel Yonov <157459005+gabrielyonov15@users.noreply.github.com>
2026-01-04 14:02:47 +0200

    Update SharedFile model: add foreign keys and navigation properties

--- LuxDrive.Data.Models/SharedFile.cs
+++ LuxDrive.Data.Models/SharedFile.cs
@@ -1,14 +1,23 @@
-﻿namespace LuxDrive.Data.Models
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LuxDrive.Data.Models
 {
     public class SharedFile
     {
         public int Id { get; set; }
-
         public Guid FileId { get; set; }
-        public File File { get; set; } = null!;
+        public virtual File File { get; set; } = null!;
 
         public Guid SenderId { get; set; }
+
+        [ForeignKey(nameof(SenderId))]
+        public virtual ApplicationUser Sender { get; set; } = null!;
+
         public Guid ReceiverId { get; set; }
+
+        [ForeignKey(nameof(ReceiverId))]
+        public virtual ApplicationUser Receiver { get; set; } = null!;
 
         public DateTime SharedOn { get; set; }
     }
