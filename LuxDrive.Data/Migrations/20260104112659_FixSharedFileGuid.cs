5f08749be2823cc57c6fc41c7a2a71219df58d07
Gabriel Yonov <157459005+gabrielyonov15@users.noreply.github.com>
2026-01-04 14:06:53 +0200

    Add migration to enforce Guid foreign keys and Restrict delete behavior

---
+++ LuxDrive.Data/Migrations/20260104112659_FixSharedFileGuid.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LuxDrive.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixSharedFileGuid : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_SharedFiles_ReceiverId",
+                table: "SharedFiles",
+                column: "ReceiverId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SharedFiles_SenderId",
+                table: "SharedFiles",
+                column: "SenderId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_SharedFiles_AspNetUsers_ReceiverId",
+                table: "SharedFiles",
+                column: "ReceiverId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_SharedFiles_AspNetUsers_SenderId",
+                table: "SharedFiles",
+                column: "SenderId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_SharedFiles_AspNetUsers_ReceiverId",
+                table: "SharedFiles");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_SharedFiles_AspNetUsers_SenderId",
+                table: "SharedFiles");
+
+            migrationBuilder.DropIndex(
+                name: "IX_SharedFiles_ReceiverId",
+                table: "SharedFiles");
+
+            migrationBuilder.DropIndex(
+                name: "IX_SharedFiles_SenderId",
+                table: "SharedFiles");
+        }
+    }
+}
