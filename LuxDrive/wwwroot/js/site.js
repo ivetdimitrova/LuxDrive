0518575722e76f6f85a1e3b22d127fb3c4e1fde0
Gabriel Yonov <157459005+gabrielyonov15@users.noreply.github.com>
2026-01-10 18:11:59 +0200

    feat(js): implement auto-dismiss logic for success alerts

---
+++ LuxDrive/wwwroot/js/site.js
@@ -0,0 +1,13 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+
+    const alert = document.getElementById('success-alert');
+
+    if (alert) {
+        setTimeout(function () {
+            alert.style.opacity = '0';
+            alert.style.transform = 'translate(-50%, -20px)';
+
+            setTimeout(() => alert.remove(), 500);
+        }, 4000);
+    }
+});
