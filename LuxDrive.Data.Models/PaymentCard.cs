71e305361f4bd95f2fcf58378eb08cc9154f5500
Gabriel Yonov <157459005+gabrielyonov15@users.noreply.github.com>
2026-01-11 19:10:07 +0200

    feat(models): add PaymentCard entity

---
+++ LuxDrive.Data.Models/PaymentCard.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LuxDrive.Data.Models
+{
+    public class PaymentCard
+    {
+        public int Id { get; set; }
+
+        public string UserId { get; set; } 
+
+        public string CardLast4 { get; set; } 
+        public string CardType { get; set; } 
+    }
+}
