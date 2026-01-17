db29336c9c201e20dfc2efa03d293dcec18f1407
Gabriel Yonov <157459005+gabrielyonov15@users.noreply.github.com>
2026-01-17 18:53:07 +0200

    feat: implement trash bin functionality and UI

---
+++ LuxDrive/Views/File/Trash.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LuxDrive.Views.File
+{
+    public class TrashModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
