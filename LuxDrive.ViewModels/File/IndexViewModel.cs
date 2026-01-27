b56d8ccf748474872c2807e97a61e48efe02c102
iamivet <159651783+ivetdimitrova@users.noreply.github.com>
2026-01-27 18:31:55 +0200

    Creating a viewModel for File/Index

---
+++ LuxDrive.ViewModels/File/IndexViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LuxDrive.ViewModels.File
+{
+    public class IndexViewModel
+    {
+        public Guid Id { get; set; }
+        public string Type { get; set; } = string.Empty;
+        public string StorageUrl { get; set; } = string.Empty;
+        public string Icon { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+        public string Extension { get; set; } = string.Empty;
+        public bool IsDeleted { get; set; }
+        public int Size { get; set; }
+        public DateTime UploadedAt { get; set; }
+    }
+}
