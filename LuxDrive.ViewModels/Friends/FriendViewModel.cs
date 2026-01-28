4495ccdb7c573ae882f2e2e290712a77350927c4
iamivet <159651783+ivetdimitrova@users.noreply.github.com>
2026-01-28 12:18:13 +0200

    Create FriendViewModel

---
+++ LuxDrive.ViewModels/Friends/FriendViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LuxDrive.ViewModels.Friends
+{
+    public class FriendViewModel
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+    }
+}
