c70a24c77c2486f7b6e5b8901b84b08d984c9a02
iamivet <159651783+ivetdimitrova@users.noreply.github.com>
2026-01-29 10:41:53 +0200

    Create FriendViewModel for _FriendsModalPartial

---
+++ LuxDrive.ViewModels/Friends/FriendsMainViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LuxDrive.ViewModels.Friends
+{
+    public class FriendsMainViewModel
+    {
+        public IEnumerable<FriendViewModel> Friends { get; set; } 
+        public IEnumerable<RequestViewModel> Requests { get; set; }
+    }
+}
