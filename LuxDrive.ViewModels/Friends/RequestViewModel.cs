7e0ff79a525494b8a7487bec19e3d10c24011787
iamivet <159651783+ivetdimitrova@users.noreply.github.com>
2026-01-29 10:40:46 +0200

    Create RequestViewModel  for part of _FriendsModalPartial

---
+++ LuxDrive.ViewModels/Friends/RequestViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LuxDrive.ViewModels.Friends
+{
+    public class RequestViewModel
+    {
+        public Guid Id { get; set; }
+
+        public string SenderName { get; set; } = string.Empty;
+    }
+}
