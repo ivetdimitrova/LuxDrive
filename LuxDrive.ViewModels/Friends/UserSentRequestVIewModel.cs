05712f39c3a442967e6f3cc6548c1e8eaf7c5060
iamivet <159651783+ivetdimitrova@users.noreply.github.com>
2026-01-30 11:28:44 +0200

    Create UserSentRequestViewModel

---
+++ LuxDrive.ViewModels/Friends/UserSentRequestVIewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LuxDrive.ViewModels.Friends
+{
+    public class UserSentRequestVIewModel
+    {
+        public Guid Id { get; set; }
+        public int Status { get; set; }
+        public string ReceiverName { get; set; } = string.Empty;
+    }
+}
