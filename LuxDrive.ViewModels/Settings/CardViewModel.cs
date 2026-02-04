bdac3a8c7c2cf356fb7e16add7aa9b80ad4c13ef
iamivet <159651783+ivetdimitrova@users.noreply.github.com>
2026-02-04 17:31:38 +0200

    Create CardViewModel for the card collection in UserSettingsViewModel

---
+++ LuxDrive.ViewModels/Settings/CardViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LuxDrive.ViewModels.Settings
+{
+    public class CardViewModel
+    {
+        public Guid Id { get; set; }
+        public string CardNumber { get; set; }
+        public string CardLast4 { get; set; }
+        public string CardType { get; set; }
+    }
+}
