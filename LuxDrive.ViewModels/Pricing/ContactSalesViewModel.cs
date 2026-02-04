d72000eb544c915e789334c29f1958c7443330cc
Gabriel Yonov <157459005+gabrielyonov15@users.noreply.github.com>
2026-02-04 17:44:52 +0200

    Add ContactSalesViewModel with validation for enterprise inquiries

---
+++ LuxDrive.ViewModels/Pricing/ContactSalesViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LuxDrive.ViewModels.Pricing
+{
+    public class ContactSalesViewModel
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        public string? Company { get; set; }
+
+        [Required]
+        public string Message { get; set; }
+    }
+}
