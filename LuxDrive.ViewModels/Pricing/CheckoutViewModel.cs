fedb0b8800046ea92a04c38790508e41abddb5b4
Gabriel Yonov <157459005+gabrielyonov15@users.noreply.github.com>
2026-02-06 17:30:23 +0200

    feat(pricing): enhance checkout validation and localize error messages

--- LuxDrive.ViewModels/Pricing/CheckoutViewModel.cs
+++ LuxDrive.ViewModels/Pricing/CheckoutViewModel.cs
@@ -6,19 +6,20 @@ namespace LuxDrive.ViewModels.Pricing
     {
         public string PlanName { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Card name is required")]
+        [RegularExpression(@"^[a-zA-Z\s\-]+$", ErrorMessage = "Name must contain only letters")]
         public string CardName { get; set; }
 
-        [Required]
-        [StringLength(19)] 
+        [Required(ErrorMessage = "Card number is required")]
+        [RegularExpression(@"^(\d{4}\s){3}\d{4}$|^(\d{16})$", ErrorMessage = "Invalid card format (16 digits)")]
         public string CardNumber { get; set; }
 
-        [Required]
-        [StringLength(5)] 
+        [Required(ErrorMessage = "Expiry date is required")]
+        [RegularExpression(@"^(0[1-9]|1[0-2])\/([0-9]{2})$", ErrorMessage = "Format must be MM/YY")]
         public string Expiry { get; set; }
 
-        [Required]
-        [StringLength(3)]
+        [Required(ErrorMessage = "CVC is required")]
+        [RegularExpression(@"^[0-9]{3,4}$", ErrorMessage = "CVC must be 3 digits")]
         public string CVC { get; set; }
 
         public string PriceDisplay => PlanName?.ToLower() switch
