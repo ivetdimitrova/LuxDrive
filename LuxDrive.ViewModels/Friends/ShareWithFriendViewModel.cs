ed63a4e519f063cae430bff9637d9cde422cf8f5
iamivet <159651783+ivetdimitrova@users.noreply.github.com>
2026-02-12 22:27:52 +0200

    Add file sharing functionality

---
+++ LuxDrive.ViewModels/Friends/ShareWithFriendViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LuxDrive.ViewModels.Friends
+{
+    public class ShareWithFriendViewModel
+    {
+        public Guid FileId { get; set; }
+        public Guid ReceiverId { get; set; }
+        public IEnumerable<FriendViewModel> Friends { get; set; }
+
+    }
+}
