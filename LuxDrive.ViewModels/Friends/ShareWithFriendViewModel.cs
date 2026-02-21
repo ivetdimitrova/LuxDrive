8dd559edc42ebe0a2cd14d23ad38c55bb447fbe1
Gabriel Yonov <157459005+gabrielyonov15@users.noreply.github.com>
2026-02-21 14:14:22 +0200

    feat: add validation to ShareWithFriendViewModel

--- LuxDrive.ViewModels/Friends/ShareWithFriendViewModel.cs
+++ LuxDrive.ViewModels/Friends/ShareWithFriendViewModel.cs
@@ -1,16 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace LuxDrive.ViewModels.Friends
 {
     public class ShareWithFriendViewModel
     {
+        [Required]
         public Guid FileId { get; set; }
-        public Guid ReceiverId { get; set; }
-        public IEnumerable<FriendViewModel> Friends { get; set; }
 
+        [Required(ErrorMessage = "Please select a friend to share with.")]
+        public Guid ReceiverId { get; set; }
+
+        public IEnumerable<FriendViewModel> Friends { get; set; }
     }
 }
