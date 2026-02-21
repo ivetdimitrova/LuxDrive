5c6d7d0006321674b044de59e9d843f7a321dba9
Gabriel Yonov <157459005+gabrielyonov15@users.noreply.github.com>
2026-02-21 14:13:48 +0200

    feat: add data validation to FriendViewModel

--- LuxDrive.ViewModels/Friends/FriendViewModel.cs
+++ LuxDrive.ViewModels/Friends/FriendViewModel.cs
@@ -1,17 +1,20 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Globalization;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace LuxDrive.ViewModels.Friends
 {
     public class FriendViewModel
     {
         public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "Name is required.")]
+        [StringLength(50, MinimumLength = 2, ErrorMessage = "Name must be between 2 and 50 characters.")]
         public string Name { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Email is required.")]
+        [EmailAddress(ErrorMessage = "Invalid email address format.")]
         public string Email { get; set; } = string.Empty;
+
         public string? ProfileImageUrl { get; set; }
     }
 }
