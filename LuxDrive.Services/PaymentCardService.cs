e628f8a81eb633b202d266c8381f1955c0119197
iamivet <159651783+ivetdimitrova@users.noreply.github.com>
2026-02-28 12:59:49 +0200

    Add comments in PaymentCardService

--- LuxDrive.Services/PaymentCardService.cs
+++ LuxDrive.Services/PaymentCardService.cs
@@ -16,6 +16,16 @@ namespace LuxDrive.Services
             _context = context;
         }
 
+        /*
+        <summary>
+        Метод за добавяне на нова разплащателна карта към профила на потребителя.
+        Проверява дали данните са валидни и дали същата карта (със същите последни 4 цифри) вече не е добавена, преди да запише новата информация в базата.
+        </summary>
+        <param name="userId">Id-то на потребителя, който добавя картата.</param>
+        <param name="last4">Последните четири цифри на картата.</param>
+        <param name="cardType">Типът на картата (напр. Visa, Mastercard).</param>
+        <exception cref="ArgumentException">Гърми, ако Id-то е невалидно или цифрите на картата не са точно 4.</exception>
+        */
         public async Task CreateCardAsync(Guid userId, string last4, string cardType)
         {
             if (userId == Guid.Empty)
@@ -41,6 +51,15 @@ namespace LuxDrive.Services
             }
         }
 
+        /*
+        <summary>
+        Метод за изтриване на конкретна разплащателна карта от профила.
+        Намира картата по нейното Id и проверява дали тя наистина принадлежи на потребителя, преди да я премахне окончателно от базата данни.
+        </summary>
+        <param name="cardId">Уникалното Id на картата за изтриване.</param>
+        <param name="userId">Id-то на потребителя под формата на текст.</param>
+        <exception cref="ArgumentException">Гърми, ако Id-то на потребителя е грешно или картата не съществува.</exception>
+        */
         public async Task DeleteCardAsync(Guid cardId, string userId)
         {
 
@@ -59,6 +78,15 @@ namespace LuxDrive.Services
 
         }
 
+        /*
+        <summary>
+        Метод за извличане на списък с всички карти, свързани с профила на даден потребител.
+        Връща само необходимата информация (Id, последни цифри и тип) за визуализация в настройките на профила.
+        </summary>
+        <param name="userId">Уникалното Id на потребителя.</param>
+        <returns>Списък с модели на картите на потребителя.</returns>
+        <exception cref="ArgumentException">Гърми, ако Id-то на потребителя е невалидно.</exception>
+        */
         public async Task<List<CardViewModel>?> GetUserCardsAsync(Guid userId)
         {
 
@@ -76,6 +104,15 @@ namespace LuxDrive.Services
                                       .ToListAsync();
         }
 
+        /*
+        <summary>
+        Метод за бърза проверка дали даден потребител има поне една свързана карта.
+        Използва се често за логика, свързана с абонаменти или плащания, за да се разбере дали потребителят е настроил платежен метод.
+        </summary>
+        <param name="userId">Уникалното Id на потребителя.</param>
+        <returns>Връща true, ако има поне една карта, иначе false.</returns>
+        <exception cref="ArgumentException">Гърми, ако Id-то на потребителя е невалидно.</exception>
+        */
         public async Task<bool> HasUserLinkedCardAsync(Guid userId)
         {
             if (userId == Guid.Empty)
