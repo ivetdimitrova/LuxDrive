0e62ff3be214992c11c2ac58ed517bf13a7b4bc5
iamivet <159651783+ivetdimitrova@users.noreply.github.com>
2026-02-28 12:31:39 +0200

    Change FIleService comments

--- LuxDrive.Services/FileService.cs
+++ LuxDrive.Services/FileService.cs
@@ -20,8 +20,8 @@ namespace LuxDrive.Services
 
         /*
         <summary>  Метод за смяна на името на даден файл. </summary>
-        <param name="userId">Айдито на човека, който притежава файла.</param>
-        <param name="fileId">Айдито на самия файл, който преименуваме.</param>
+        <param name="userId">Id-то на човека, който притежава файла.</param>
+        <param name="fileId">Id-то на самия файл, който преименуваме.</param>
         <param name="newName">Как искаме да се казва файлът вече.</param>
         <exception cref="ArgumentException">Гърми, ако името е празно, потребителят е невалиден или файлът не съществува.</exception>
          */
@@ -61,8 +61,8 @@ namespace LuxDrive.Services
         }
 
 
-        /* <summary>Метод за създаване на нов запис за файл в базата.</summary>
-        <param name="userId">Айдито на човека, който качва файла.</param>
+        /* <summary>Метод за създаване на нов запис на файл в базата.</summary>
+        <param name="userId">Id-то на човека, който качва файла.</param>
         <param name="file">Самият файл, който идва от формата за качване.</param>
         <returns>Връща Айдито на току-що създадения файл. Ако няма такъв файл, връща null.</returns>
         <exception cref="ArgumentException">Гърми, ако Айдито на потребителя не е валидно.</exception>
@@ -96,12 +96,12 @@ namespace LuxDrive.Services
 
 
         /* 
-        <summary>Методът намира и връща разширението на конкретен файл по неговото Айди.
-        Проверява дали подаденото Айди е валидно и ако всичко е наред, дърпа само разширението от базата данни, без да следи обекта за промени.
+        <summary>Метод за намиране и връщане на разширението на конкретен файл по неговото Id.
+        Проверява дали подаденото Id е валидно и ако всичко е наред, дърпа само разширението от базата данни, без да следи обекта за промени.
         </summary>
-        <param name="fileId">Уникалното Айди на файла, чието разширение ни трябва.</param>
+        <param name="fileId">Уникалното Id на файла, чието разширение ни трябва.</param>
         <returns>Връща разширението като текст (например ".jpg") или null, ако не намери такъв файл.</returns>
-        <exception cref="ArgumentException">Хвърля грешка, ако Айдито е празно или невалидно.</exception>
+        <exception cref="ArgumentException">Хвърля грешка, ако Id-то е празно или невалидно.</exception>
         */
 
         public async Task<string?> GetFileExtensionAsync(Guid? fileId)
@@ -118,10 +118,10 @@ namespace LuxDrive.Services
 
 
         /* 
-        <summary> Взима всички файлове на даден потребител, които не са изтрити. 
-        Методът попълва данните за всеки файл и му слага подходяща иконка (снимка, музика, документ и т.н.) според разширението му, за да се виждат красиво в списъка.
+        <summary> Метод за взимане на всички файлове на даден потребител, които не са изтрити. 
+        Попълва данните за всеки файл и му слага подходяща иконка (снимка, музика, документ и т.н.) според разширението му, за да се виждат красиво в списъка.
         </summary>
-        <param name="userId">Айдито на потребителя, чиито файлове търсим.</param>
+        <param name="userId">Id-то на потребителя, чиито файлове търсим.</param>
         <returns>Връща списък с файловете, готови за показване на екрана.</returns>
         <exception cref="ArgumentException">Хвърля грешка, ако Айдито на потребителя е грешно.</exception>
         */
@@ -202,12 +202,12 @@ namespace LuxDrive.Services
 
 
         /* 
-        <summary>Методът обновява интернет адреса (URL) на вече съществуващ файл. 
-        Проверява дали подаденото Айди и новият линк са валидни, намира файла в базата и му записва новия адрес.
+        <summary>Метод за обновяване на интернет адреса (URL) на вече съществуващ файл. 
+        Проверява дали подаденото Id и новият линк са валидни, намира файла в базата и му записва новия адрес.
         </summary>
-        <param name="fileId">Уникалното Айди на файла, който ще обновяваме.</param>
+        <param name="fileId">Уникалното Id на файла, който ще обновяваме.</param>
         <param name="url">Новият адрес, на който е качен файлът.</param>
-        <exception cref="ArgumentException">Хвърля грешка, ако Айдито е невалидно, линкът е празен или файлът не е намерен.</exception>
+        <exception cref="ArgumentException">Хвърля грешка, ако Id-то е невалидно, линкът е празен или файлът не е намерен.</exception>
          */
         public async Task UpdateFileUrlAsync(Guid? fileId, string url)
         {
@@ -226,17 +226,17 @@ namespace LuxDrive.Services
             await _dbContext.SaveChangesAsync();
         }
 
-       /* 
-        <summary>Методът служи за споделяне на файл с друг потребител.
-        Проверява дали двамата потребители са приятели и дали файлът вече не е бил споделен с този човек. 
-        Ако всичко е точно, прави нов запис в базата данни за споделения файл.
-        </summary>
-        <param name="fileId">Айдито на файла, който се споделя.</param>
-        <param name="senderId">Айдито на човека, който изпраща файла.</param>
-        <param name="receiverId">Айдито на човека, който ще получи файла.</param>
-        <exception cref="ArgumentException">Хвърля грешка при невалидно Айди на изпращача.</exception>
-        <exception cref="InvalidOperationException">Хвърля грешка, ако потребителите не са приятели.</exception>
-        */
+        /* 
+         <summary>Метод за споделяне на файл с друг потребител.
+         Проверява дали двамата потребители са приятели и дали файлът вече не е бил споделен с този човек. 
+         Ако всичко е точно, прави нов запис в базата данни за споделения файл.
+         </summary>
+         <param name="fileId">Id-то на файла, който се споделя.</param>
+         <param name="senderId">Id-то на човека, който изпраща файла.</param>
+         <param name="receiverId">Id-то на човека, който ще получи файла.</param>
+         <exception cref="ArgumentException">Хвърля грешка при невалидно Id на изпращача.</exception>
+         <exception cref="InvalidOperationException">Хвърля грешка, ако потребителите не са приятели.</exception>
+         */
         public async Task ShareFileAsync(Guid fileId, string senderId, Guid receiverId)
         {
             if (!Guid.TryParse(senderId, out Guid senderGuid))
@@ -268,12 +268,12 @@ namespace LuxDrive.Services
 
         /*
          <summary>
-         Методът взима всички файлове, които са били споделени с потребителя от други хора.
-         Проверява  Айдито му, намира кои файлове са пратени към него и записва информацията за тях (име, линк и кой точно ги е пратил).
+         Метод за взимане на всички файлове, които са били споделени с потребителя от други хора.
+         Проверява  Id-то му, намира кои файлове са пратени към него и записва информацията за тях (име, линк и кой точно ги е пратил).
          </summary>
-         <param name="userId">Айдито на потребителя, който си гледа споделените файлове.</param>
+         <param name="userId">Id-то на потребителя, който си гледа споделените файлове.</param>
          <returns>Списък с файловете, които са ти изпратени, заедно с името на изпращача.</returns>
-         <exception cref="ArgumentException">Хвърля грешка, ако Айдито на потребителя не е валидно.</exception>
+         <exception cref="ArgumentException">Хвърля грешка, ако Id-то на потребителя не е валидно.</exception>
         */
         public async Task<IEnumerable<IndexViewModel>> GetSharedWithMeFilesAsync(string userId)
         {
@@ -299,12 +299,12 @@ namespace LuxDrive.Services
 
         /*
         <summary>
-        Методът смята колко общо място заемат всички файлове на потребителя.
+        Метод за пресмятане на колко общо място заемат всички файлове на потребителя.
         Събира размерите на всеки негов файл от базата данни и връща общата сума. Ако потребителят няма качени файлове, връща 0.
         </summary>
-        <param name="userId">Айдито на човека, за когото проверяваме колко памет е заел.</param>
+        <param name="userId">Id-то на човека, за когото проверяваме колко памет е заел.</param>
         <returns>Общият размер на всички файлове като число.</returns>
-        <exception cref="ArgumentException">Гърми, ако Айдито на потребителя е невалидно.</exception>
+        <exception cref="ArgumentException">Гърми, ако Id-то на потребителя е невалидно.</exception>
         */
         public async Task<long> GetTotalUsedStorageAsync(string userId)
         {
@@ -323,8 +323,8 @@ namespace LuxDrive.Services
         Вместо да го трие завинаги, той просто го маркира като изтрит в базата и записва кога точно се е случило това. 
         Файлът трябва да принадлежи на потребителя, който се опитва да го изтрие.
         </summary>
-        <param name="id">Уникалното Айди на файла, който ще махаме.</param>
-        <param name="userIdStr">Айдито на потребителя под формата на текст.</param>
+        <param name="id">Уникалното Id на файла, който ще махаме.</param>
+        <param name="userIdStr">Id-то на потребителя под формата на текст.</param>
         <exception cref="ArgumentException">Хвърля грешка, ако потребителят е невалиден или ако файлът вече е изтрит или не съществува.</exception>
         */
         public async Task DeleteUserFileAsync(Guid id, string userIdStr)
@@ -348,9 +348,9 @@ namespace LuxDrive.Services
         Метод за възстановяване на изтрит файл. 
         Намира файла в базата и премахва отметката за изтриване, като го прави отново видим за потребителя.
         </summary>
-        <param name="id">Уникалното Айди на файла, който да върнем.</param>
-        <param name="userIdStr">Айдито на потребителя, на когото принадлежи файлът.</param>
-        <exception cref="ArgumentException">Хвърля грешка, ако Айди-тата са невалидни или файлът изобщо не съществува.</exception>
+        <param name="id">Уникалното Id на файла, който да върнем.</param>
+        <param name="userIdStr">Id-то на потребителя, на когото принадлежи файлът.</param>
+        <exception cref="ArgumentException">Хвърля грешка, ако Id-тата са невалидни или файлът изобщо не съществува.</exception>
         */
 
         public async Task RestoreUserFileAsync(Guid id, string userIdStr)
@@ -378,12 +378,12 @@ namespace LuxDrive.Services
 
         /*
         <summary>
-        Методът показва всички файлове на потребителя, които са в кошчето (маркирани като изтрити).
+        Метод за показване на всички файлове на потребителя, които са в кошчето (маркирани като изтрити).
         Подрежда ги така, че най-скоро изтритите да са най-отгоре, и взима само важната информация за тях като име, разширение и кога точно са били махнати.
         </summary>
-        <param name="userIdStr">Айдито на потребителя, който проверява кошчето си.</param>
+        <param name="userIdStr">Id-то на потребителя, който проверява кошчето си.</param>
         <returns>Списък с изтритите файлове, готови за показване в изгледа на кошчето.</returns>
-        <exception cref="ArgumentException">Хвърля грешка, ако Айдито на потребителя не е правилно.</exception>
+        <exception cref="ArgumentException">Хвърля грешка, ако Id-то на потребителя не е правилно.</exception>
         */
         public async Task<IEnumerable<TrashViewModel>?> GetTrashedFilesAsync(string userIdStr)
         {
@@ -408,14 +408,14 @@ namespace LuxDrive.Services
 
         /*
         <summary>
-        Методът окончателно изтрива файл от системата.
+        Метод за окончателно изтриване на файл от системата.
         Проверява дали файлът съществува и дали принадлежи на човека, който иска да го изтрие. 
         Ако всичко е наред, изтрива записа от базата данни завинаги и връща адреса на файла, за да може после да се изтрие и от облака (storage-а).
         </summary>
-        <param name="id">Уникалното Айди на файла, който ще се трие завинаги.</param>
-        <param name="userIdStr">Айдито на потребителя под формата на текст.</param>
+        <param name="id">Уникалното Id на файла, който ще се трие завинаги.</param>
+        <param name="userIdStr">Id-то на потребителя под формата на текст.</param>
         <returns>Връща линка към файла в облака, за да бъде премахнат и оттам. Ако файлът не е намерен, връща null.</returns>
-        <exception cref="ArgumentException">Гърми, ако Айдито на файла или на потребителя е невалидно.</exception>
+        <exception cref="ArgumentException">Гърми, ако Id-то на файла или на потребителя е невалидно.</exception>
         */
         public async Task<string?> PermanentDeleteFileAsync(Guid id, string userIdStr)
         {
@@ -443,14 +443,14 @@ namespace LuxDrive.Services
 
         /*
         <summary>
-        Методът „меко“ изтрива няколко файла едновременно (преместване в кошчето).
+        Метод за „меко“ изтриване на няколко файла едновременно (преместване в кошчето).
         Проверява кои от дадените файлове наистина принадлежат на потребителя и не са вече изтрити.
         Маркира ги всичките като "изтрити" и записва часа на изтриването.
         </summary>
-        <param name="ids">Списък с Айдитата на всички файлове, които искаме да махнем.</param>
-        <param name="userIdStr">Айдито на потребителя под формата на текст.</param>
+        <param name="ids">Списък с Id-тата на всички файлове, които искаме да махнем.</param>
+        <param name="userIdStr">Id-то на потребителя под формата на текст.</param>
         <returns>Връща true, ако е успял да изтрие поне един файл, иначе връща false.</returns>
-        <exception cref="ArgumentException">Гърми, ако Айдито на потребителя е грешно.</exception>
+        <exception cref="ArgumentException">Гърми, ако Id-то на потребителя е грешно.</exception>
         */
         public async Task<bool> DeleteMultipleFilesAsync(List<Guid> ids, string userIdStr)
         {
@@ -481,10 +481,10 @@ namespace LuxDrive.Services
         Метод за споделяне на група от файлове наведнъж с друг потребител.
         Проверява дали получателят е валиден и след това завърта цикъл, за да извика основния метод за споделяне за всеки файл от списъка.
         </summary>
-        <param name="ids">Списък с Айдитата на всички файлове, които искаме да пратим.</param>
-        <param name="userIdStr">Айдито на човека, който споделя файловете.</param>
-        <param name="receiverId">Айдито на човека, който ще ги получи.</param>
-        <exception cref="ArgumentException">Гърми, ако Айдито на получателя е празно или невалидно.</exception>
+        <param name="ids">Списък с Id-тата на всички файлове, които искаме да пратим.</param>
+        <param name="userIdStr">Id-то на човека, който споделя файловете.</param>
+        <param name="receiverId">Id-то на човека, който ще ги получи.</param>
+        <exception cref="ArgumentException">Гърми, ако Id-то на получателя е празно или невалидно.</exception>
         */
 
         public async Task ShareMultipleFilesAsync(List<Guid> ids, string userIdStr, Guid receiverId)
@@ -507,9 +507,9 @@ namespace LuxDrive.Services
         Намира всички файлове, които са отбелязани като изтрити, и ги премахва окончателно от базата данни. 
         Накрая връща списък с техните адреси (URL), за да могат да бъдат изтрити и от самото облачно хранилище.
         </summary>
-        <param name="userIdStr">Айдито на потребителя, който си чисти кошчето.</param>
+        <param name="userIdStr">Id-то на потребителя, който си чисти кошчето.</param>
         <returns>Списък с адресите на всички изтрити файлове или null, ако кошчето е вече празно.</returns>
-        <exception cref="ArgumentException">Гърми, ако Айдито на потребителя не е валидно.</exception>
+        <exception cref="ArgumentException">Гърми, ако Id-то на потребителя не е валидно.</exception>
         */
 
         public async Task<List<string>?> EmptyTrashAsync(string userIdStr)
@@ -547,10 +547,10 @@ namespace LuxDrive.Services
         Метод за възстановяване на няколко файла едновременно от кошчето.
         Проверява кои от избраните файлове наистина са в кошчето и принадлежат на потребителя, след което ги прави отново активни (премахва датата на изтриване).
         </summary>
-        <param name="ids">Списък с Айдитата на файловете, които искаме да върнем.</param>
-        <param name="userIdStr">Айдито на потребителя под формата на текст.</param>
+        <param name="ids">Списък с Id-тата на файловете, които искаме да върнем.</param>
+        <param name="userIdStr">Id-то на потребителя под формата на текст.</param>
         <returns>Връща true, ако е успял да възстанови поне един файл, иначе връща false.</returns>
-        <exception cref="ArgumentException">Гърми, ако списъкът с файлове е празен или Айдито на потребителя е грешно.</exception>
+        <exception cref="ArgumentException">Гърми, ако списъкът с файлове е празен или Id-то на потребителя е грешно.</exception>
         */
         public async Task<bool> RestoreMultipleFilesAsync(List<Guid> ids, string userIdStr)
         {
@@ -578,11 +578,11 @@ namespace LuxDrive.Services
         /*
         <summary>
         Метод за подготвяне на файл за сваляне.
-        Проверява дали Айдитата са точни и намира файла в базата (стига да не е изтрит). 
+        Проверява дали Id-тата са точни и намира файла в базата (стига да не е изтрит). 
         Връща само най-важното: името, разширението и линка към мястото, където се съхранява файлът, за да може потребителят да си го изтегли.
         </summary>
-        <param name="id">Уникалното Айди на файла, който искаме да свалим.</param>
-        <param name="userIdStr">Айдито на потребителя, който прави заявката.</param>
+        <param name="id">Уникалното Id на файла, който искаме да свалим.</param>
+        <param name="userIdStr">Id-то на потребителя, който прави заявката.</param>
         <returns>Връща данните за файла (име и линк) или null, ако файлът не съществува или е изтрит.</returns>
         <exception cref="ArgumentException">Гърми, ако Айдито на потребителя или на файла е невалидно.</exception>
         */
@@ -609,13 +609,13 @@ namespace LuxDrive.Services
         /*
         <summary>
         Метод за сваляне на  списък от файлове наведнъж.
-        Проверява дали подадените Айдита са валидни и намира само тези файлове, които принадлежат на потребителя и не са изтрити.
+        Проверява дали подадените Id-та са валидни и намира само тези файлове, които принадлежат на потребителя и не са изтрити.
         Връща списък с имената и адресите на файловете, за да могат да бъдат изтеглени заедно.
         </summary>
-        <param name="ids">Списък с Айдитата на файловете, които потребителят иска да свали.</param>
-        <param name="userIdStr">Айдито на потребителя, който прави заявката.</param>
+        <param name="ids">Списък с Id-тата на файловете, които потребителят иска да свали.</param>
+        <param name="userIdStr">Id-то на потребителя, който прави заявката.</param>
         <returns>Списък с данни за сваляне на всеки файл или null, ако нищо не е намерено.</returns>
-        <exception cref="ArgumentException">Гърми, ако списъкът с Айдита е празен или Айдито на потребителя е грешно.</exception>
+        <exception cref="ArgumentException">Гърми, ако списъкът с Id-та е празен или Айдито на потребителя е грешно.</exception>
         */
         public async Task<List<DownloadFileViewModel>?> GetMultipleFilesToDownloadAsync(List<Guid> ids, string userIdStr)
         {
