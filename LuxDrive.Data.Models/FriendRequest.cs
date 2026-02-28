7490d5cf8c08e3efab486db1a5fc97dba2e9510f
iamivet <159651783+ivetdimitrova@users.noreply.github.com>
2026-02-28 16:21:58 +0200

    Remove unnecessary usings

--- LuxDrive.Data.Models/FriendRequest.cs
+++ LuxDrive.Data.Models/FriendRequest.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using LuxDrive.Data.Models.Enums;
+﻿using LuxDrive.Data.Models.Enums;
 
-    namespace LuxDrive.Data.Models
+namespace LuxDrive.Data.Models
     {
         public class FriendRequest
         {
